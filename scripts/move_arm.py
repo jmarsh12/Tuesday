b778bc4cffbf5732168f3564b512cb6709f2d5b1
aamott <adam.amott@gmail.com>
2021-07-14 18:41:17 -0600

    fixed base positioning

--- scripts/move_arm.py
+++ scripts/move_arm.py
@@ -27,12 +27,12 @@
 #   Pressure hold: 700
 #   
 # Servos:
-#    3   
-#    #-#   7
-#    #   6
-#    #   5
-#    #   2
-#   _#_  4
+#    3          Wrist 2  - rotates
+#    #-#   7    Hand     - bends
+#    #   6      Wrist 1  - bends
+#    #   5      Elbow    - bends
+#    #   2      Shoulder - bends
+#   _#_  4      Base     - rotates  hip to ground: 
 #
 #################################################
 
@@ -42,20 +42,51 @@ from std_msgs.msg import String
 from std_msgs.msg import Int16MultiArray
 import time
 from math import sin, cos, tan, asin, acos, atan, degrees, sqrt # import sin, cosine, and tangent
-    
-servo_publisher = None
 
-# Servo Positions
+# Servo numbers
+BASE = 4
+SHOULDER = 2
+ELBOW = 5
+WRIST_1 = 6
+WRIST_2 = 3
+HAND = 7
+
+FRAME_WIDTH = 640 # 1280
+FRAME_HEIGHT = 480 # 720
+
+# Measurements
+# get this by placing a ruler vertically in the camera view
+CAMERA_VIEW_HEIGHT_MM = 146
+
+PIXELS_PER_MM = FRAME_HEIGHT / CAMERA_VIEW_HEIGHT_MM
+TOTAL_HEIGHT = 225 * PIXELS_PER_MM
+# Difference between the height of wrist1 and the hip
+WRIST1_TO_GROUND = 114 * PIXELS_PER_MM
+WRIST1_TO_WRIST2 = 60 * PIXELS_PER_MM
+ELBOW_TO_WRIST1 = 55 * PIXELS_PER_MM
+SHOULDER_TO_ELBOW = 55 * PIXELS_PER_MM
+SHOULDER_TO_GROUND = 50 * PIXELS_PER_MM
+ARM_HEIGHT_OFFSET = -40 * PIXELS_PER_MM
+# wrist1 to mid angle.
+WRIST1_TO_MID = WRIST1_TO_GROUND - SHOULDER_TO_GROUND
+
+# Base Servo Positions
 STRAIGHT = 520 # arm facing straight forward
 LEFT = 840
 RIGHT = 220
 
-CAMERA_WIDTH = 1280
-CAMERA_HEIGHT = 720
-ARM_HEIGHT_OFFSET = -205
+# Other servos
+SERVO_180 = 500
+SERVO_0 = SERVO_180 - 615 # This position can't be reached because the arm can't bend into itself - hence position -115
+
+# The arm's position in the frame (it sits a little outside)
+ARM_X_POS = FRAME_WIDTH / 2 # arm sits in middle
+ARM_Y_POS = ARM_HEIGHT_OFFSET # arm sits a little past the video frame
+
+servo_publisher = None
 
 # found at https://stackoverflow.com/questions/5294955/how-to-scale-down-a-range-of-numbers-with-a-known-min-and-max-value
-def scale_between(unscaled_num, new_min, new_max, old_min, old_max):
+def scale_between(unscaled_num, old_min, old_max, new_min, new_max):
   return (new_max - new_min) * (unscaled_num - old_min) / (old_max - old_min) + new_min
 
 
@@ -70,15 +101,20 @@ def calc_base_pos(adjacent, hypotenuse):
 
     return servo_pos
 
-# for triangles other than right
-def calc_angle_opposite_of_side(side, base, adjacent):
-    angle = degrees(acos((adjacent * adjacent + base * base - side * side)/(2.0 * adjacent * base)))
+# Calculate an angle of a non-right triangle
+def calc_angle_opposite_of_side(opposite, A, B):
+    print("Side 1: "+ str(opposite))
+    print("Side 2: " + str(A))
+    print("Side 3: "+ str(B))
+
+    angle = degrees(acos((A * A + B * B - opposite * opposite)/(2.0 * A * B)))
+    print("Final Angle: ", angle)
     return angle
 
 # calc_hypotenuse_right 
 # calculates the hypotenuse of a right triangle
 def calc_hypotenuse_right_triangle(adjacent, opposite):
-    hypotenuse =  sqrt((adjacent**2 + opposite**2))
+    hypotenuse =  sqrt(adjacent**2 + opposite**2)
     return hypotenuse
 
 def receive_coords(data):
@@ -102,12 +138,10 @@ def receive_coords(data):
     
     servo_publisher = rospy.Publisher('servo_control', String, queue_size=10)
     coords = data.data # [x, y]  or  "x, y"
-
-    home(servo_publisher)
     
     # convert variable to usable values
-    object_x_pos = 0.0
-    object_y_pos = 0.0
+    object_x_pos = None
+    object_y_pos = None
     if isinstance(coords, str): # if String
         object_x_pos, object_y_pos = coords.split(',')
         object_x_pos = float(object_x_pos)
@@ -119,68 +153,63 @@ def receive_coords(data):
 
     ##### Calculate Servo Positions ######
 
-    # Calculate Base Servo Angle
-    base_servo = 4 
-    arm_x_pos = CAMERA_WIDTH / 2 # arm sits in middle
-    arm_y_pos = CAMERA_HEIGHT + ARM_HEIGHT_OFFSET # arm sits a little past the video frame
+    ### Base ###
     
-    arm_to_obj_x = abs(object_x_pos - arm_x_pos) # if negative, the object is on the left when facing the arm
-    arm_to_obj_y = abs(object_y_pos - arm_y_pos) # subtracting a negative number makes the triangle bigger. Arm sits outside of square, so its offset is negative.
+    object_x_pos = 638
+    object_y_pos = 50
+    # find distance between object and base
+    arm_to_obj_x = object_x_pos - ARM_X_POS # if negative, the object is on the left when facing the arm
 
-    # create sides of the triangle that will be used to get the angle
-    arm_to_object_len = calc_hypotenuse_right_triangle(arm_to_obj_x, arm_to_obj_y)
-    vert_triangle_hypotenuse = calc_hypotenuse_right_triangle(object_x_pos, arm_to_obj_y)
-
-    ##  Aim arm at object - Base (rotating) Servo ##
-    angle_arm_to_object = calc_angle_opposite_of_side(arm_to_object_len, vert_triangle_hypotenuse, arm_x_pos )
-
-    # convert degrees to base servo position
-    base_pos = scale_between(angle_arm_to_object, 0, 180, LEFT, RIGHT)
-    base_pos = int(base_pos)
-
-    ## Reach for Object ##
-    shoulder_servo = 5 #Hip (arm base bending) Servo
-    wrist_twist_servo = 4
-    wrist_bend_servo = 3
-    elbow_servo = 6
+    arm_to_obj_y = object_y_pos - ARM_Y_POS # expand the triangle down to the arm
     
-    WRIST_LEN = 100 # TODO - add a real value. Measured in pixels
-    WRIST_HEIGHT = 100 # TODO: measure this, too.
-    SHOULDER_HEIGHT = 50 # TODO: measure this, too.  
-    SERVO_LEN = 100 # TODO: measure this, too.
+    base_to_object_len = sqrt(abs(arm_to_obj_x)**2 + arm_to_obj_y**2)
 
-    wrist_bend_to_shoulder_width = arm_to_object_len - WRIST_LEN
-    wrist_bend_to_shoulder_height = WRIST_HEIGHT - SHOULDER_HEIGHT
-    wrist_bend_to_shoulder_hypotenuse = calc_hypotenuse_right_triangle( wrist_bend_to_shoulder_width, wrist_bend_to_shoulder_height )
-    elbow_angle = calc_angle_opposite_of_side(wrist_bend_to_shoulder_hypotenuse, SERVO_LEN, SERVO_LEN)
-    wrist_angle = calc_angle_opposite_of_side(SERVO_LEN, wrist_bend_to_shoulder_hypotenuse, SERVO_LEN)
-    shoulder_angle = wrist_angle
+    # Find angle of base
+    base_angle = degrees(atan(arm_to_obj_y / abs(arm_to_obj_x)))
 
-    # TODO: Send angles to servos
+    if arm_to_obj_x >= 0: # if the object sits on the right side of the screen
+        base_angle += 90
 
-    # if publishing as int16MultiArray, send base_pos as [new_x, new_y]
-    servo_publisher.publish(str(base_servo) + ',' + str(base_pos)) 
-    time.sleep(0.3)
-    servo_publisher.publish(str(base_servo) + ',' + str(base_pos))
+    # convert angle to position
+    base_pos = int(scale_between(base_angle, 0, 180, RIGHT, LEFT))
+    # NOTE: debugging print statement 
+    # print(f"Base angle: {base_angle}\nBase Position: {base_pos}\n")
 
-    ## TODO: Top servos - reach for object ##
+    servo_publisher.publish(str(BASE) + ',' + str(base_pos))
 
-def home(servo_publisher):
+    ### Upper Arm ###
+    #    
+    #   /#--#
+    # #      #
+    # |      |\
+    # #      | \
+
+    wrist_bend_to_shoulder_width = base_to_object_len - WRIST1_TO_WRIST2
+    wrist_bend_to_shoulder_hypotenuse = sqrt( wrist_bend_to_shoulder_width**2 + WRIST1_TO_MID**2 )
+    # elbow_angle = calc_angle_opposite_of_side(wrist_bend_to_shoulder_hypotenuse, SHOULDER_TO_ELBOW, ELBOW_TO_WRIST1)
+    # wrist_angle = calc_angle_opposite_of_side(SHOULDER_TO_ELBOW, wrist_bend_to_shoulder_hypotenuse, ELBOW_TO_WRIST1)
+    # shoulder_angle = wrist_angle
+    # print("Elbow Angle: ", elbow_angle)
+    # print("Wrist 1 Angle: ", wrist_angle)
+    # print("Shoulder Angle: ", shoulder_angle)
+
+
+
+def home_arm(servo_publisher):
   servo_publisher.publish("(2,400),(3,500),(4,840),(5,500),(6,640),(7,450)") # one up from base
 
-# TODO: Call this before anything else
+# Call this before anything else
 def listener():
 
-    # create rospy node and subscribe to finger coordinates
+    # create rospy node
     rospy.init_node('move_arm', anonymous = True)
     servo_publisher = rospy.Publisher('servo_control', String, queue_size=10)
-    # rospy.Subscriber('opencv_coordinates', String, callback=test)
+
+    home_arm(servo_publisher)
+
+    # subscribe to opencv coordinates
     rospy.Subscriber('opencv_coordinates', String, callback=receive_coords)
     
-
-    # TODO: home arm
-    
-
     print("Move arm is running")
     rospy.spin()
 
